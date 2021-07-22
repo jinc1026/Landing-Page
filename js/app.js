2e7c01941a70c1f6d62bd3ac21761f25f78948fa
Jin Choe <jinc1026@hotmail.com>
2021-07-22 03:21:42 +0000

    added all functionalities

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@
  * 
 */
 
+	let navbar = document.querySelector("#navbar__list");
+	let header = document.querySelector("header");
+	let sections = document.querySelectorAll('section');
+	let navbarList;
+	let sectionID;
 
 /**
  * End Global Variables
@@ -25,7 +30,14 @@
  * 
 */
 
-
+	function createNav(section){
+		navbarList = document.createElement("li");
+		navbarList.textContent = section.getAttribute("data-nav");
+		sectionID = section.getAttribute("id");
+		navbarList.setAttribute("data-section-id", `#${sectionID}`);
+		navbarList.classList.add("menu__link");
+		navbar.appendChild(navbarList);		
+	};
 
 /**
  * End Helper Functions
@@ -34,12 +46,28 @@
 */
 
 // build the nav
-
+	sections.forEach(createNav);
+	
 
 // Add class 'active' to section when near top of viewport
-
+	function addActiveClass(section){
+		let sectionRect = section.getBoundingClientRect();
+		if(sectionRect.top >=-10 &&
+			sectionRect.top <10){
+			section.classList.add("your-active-class");
+		} else if (sectionRect.top > window.innerHeight ||
+					  sectionRect.bottom <= 0){
+			section.classList.remove("your-active-class");
+		}
+	}
 
 // Scroll to anchor ID using scrollTO event
+	function scrollToSection(){
+		document.querySelector(this.getAttribute("data-section-id")).scrollIntoView({behavior:"smooth"});
+		header.classList.add("hide");
+		console.log(window.scrollY);
+		console.log(this.scrollTop);
+	};
 
 
 /**
@@ -48,10 +76,21 @@
  * 
 */
 
-// Build menu 
+// Build menu - Place an item for menu
+// Expand the nav upon click
+	let menuButton = document.querySelector("#menu");
+	menuButton.addEventListener("click", function(){
+		header.classList.remove("hide");
+	})
 
 // Scroll to section on link click
+	let navbarLinks = document.querySelectorAll('li');
+	console.log("navbarLInks called");
+	navbarLinks.forEach(function(link){
+		link.addEventListener("click", scrollToSection);
+	});
 
 // Set sections as active
-
-
+	window.addEventListener("scroll", () => {
+		sections.forEach(addActiveClass);
+	});
